861926933e2798f98e845dab7751f11c41427321
rick haag <rick_haag@hotmail.com>
2022-11-09 00:00:29 -0700

    carousel improvements

--- projects/public-client/src/app/components/carousel/carousel.component.ts
+++ projects/public-client/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostBinding, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
@@ -6,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  @Input() cubeSize: number = 30;
+  @Input() faceLabels: string[] = ['A', 'B', 'C', 'D', 'E'];
+  public faces: Observable<string>[] = [];
+  //public facePositions: 
+
+  @HostBinding('style.--transform1') transform1: string = `translate3d(${-1.207 * this.cubeSize + 'vw'}, 0, 0) rotateY(-90deg)`;
+
 
   public rotate: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.faceLabels.length < 5) {
+
+    } else {
+      this.faces = this.faceLabels.map(f => of(f));
+    }
   }
 
 }
