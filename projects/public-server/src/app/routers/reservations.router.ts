c062a94fd49fa995c0ccae580ce7015c9c9b4c6b
Mike Vondrak <mvondrak@gmail.com>
2023-02-25 18:00:06 -0700

    WIP test email via nodemailer

--- projects/public-server/src/app/routers/reservations.router.ts
+++ projects/public-server/src/app/routers/reservations.router.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import nodemailer, { Transporter, TransportOptions } from 'nodemailer';
 
 import { BaseRouter } from './base.router';
 import { GenericResponse, RouterCallback } from '../models/router.model';
 import { routes } from '../routes';
+import { environment } from '../environments/environment';
+import { MailOptions } from 'nodemailer/lib/json-transport';
 
 export class ReservationsRouter extends BaseRouter {
 
@@ -12,7 +15,27 @@ export class ReservationsRouter extends BaseRouter {
 
     this.router.post(route, (req: Request, res: Response) => {
       console.log('REQUEST body', req.body);
+      
       // TODO - convert reservation data to email and send to Dive and customer
+      
+      const transporter: Transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'reservations@diveinndenver.com',
+          pass: environment.gmail_app_access_key,
+        }
+      });
+      const options: MailOptions = {
+        to: 'mike@diveinndenver.com',
+        from: 'reservations@diveinndenver.com',
+        subject: 'Test Subject',
+        text: 'Test Text',
+      }
+
+      transporter.sendMail(options, (error, info) => {
+        console.log('MAIL SENT', error, info);
+      });
+
       res.status(200).send('{ "message": "Success!" }');
 
       //routerCallback(route, res);
