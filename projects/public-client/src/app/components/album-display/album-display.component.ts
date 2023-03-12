0c0c6c95c935cdcb14521cba8353bdf3f9a517b8
rick haag <rick_haag@hotmail.com>
2023-03-11 17:03:41 -0700

    image-display
    Co-authored-by: Mike <mvondrak@gmail.com>

--- projects/public-client/src/app/components/album-display/album-display.component.ts
+++ projects/public-client/src/app/components/album-display/album-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Observable, Subject, Subscription, timer } from 'rxjs';
 import { take, takeUntil } from 'rxjs/operators';
 
@@ -7,7 +7,8 @@ import { AlbumDisplayImagePosition } from '../../models/album-display.model';
 @Component({
   selector: 'app-album-display',
   templateUrl: './album-display.component.html',
-  styleUrls: ['./album-display.component.scss']
+  styleUrls: ['./album-display.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlbumDisplayComponent implements OnInit {
   @Input() showInfo: boolean = false;
@@ -23,7 +24,7 @@ export class AlbumDisplayComponent implements OnInit {
   private imgTimer$: Observable<number> = timer(this.imgTransitionTime, this.imgTransitionTime);
   private pauseTimer$: Observable<number> = timer(this.imgTransitionTime, this.imgTransitionTime);
   private killImgTimer$ = new Subject<void>();
-  // private timerSub: Subscription;
+  private timerSub: Subscription = new Subscription;
 
   /**
    * Instead of using the image position array and getting into ElementRef and nativeElement
@@ -36,11 +37,11 @@ export class AlbumDisplayComponent implements OnInit {
     { x: 0, y: -600 },
   ];
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     // TODO set up unsubscribe
-    this.subscribeToImageTimer();
+    this.timerSub = this.subscribeToImageTimer();
   }
 
   public getImagePosition(index: number): AlbumDisplayImagePosition {
@@ -72,6 +73,7 @@ export class AlbumDisplayComponent implements OnInit {
     // $event.preventDefault();
     this.userInteracted();
     this.activeImage = idx;
+    this.cdr.detectChanges();
   }
 
   public onImageClick($event: Event) {
@@ -88,11 +90,13 @@ export class AlbumDisplayComponent implements OnInit {
   }
 
   private incrementActiveImage() {
+    console.log('!!!!!!!!!!', this.activeImage, this.imageUrls.length);
     if (this.activeImage < this.imageUrls.length - 1) {
       this.activeImage++;
     } else {
       this.activeImage = 0;
     }
+    this.cdr.detectChanges();
   }
   private decrementActiveImage() {
     if (this.activeImage > 0) {
@@ -100,16 +104,20 @@ export class AlbumDisplayComponent implements OnInit {
     } else {
       this.activeImage = this.imageUrls.length - 1;
     }
+    this.cdr.detectChanges();
   }
 
-  private subscribeToImageTimer() {
-    this.imgTimer$.pipe(takeUntil(this.killImgTimer$)).subscribe(tick => {
+  private subscribeToImageTimer(): Subscription {
+    return this.imgTimer$.pipe(takeUntil(this.killImgTimer$)).subscribe(tick => {
       console.log('tick')
       this.incrementActiveImage();
     })
   }
 
   ngOnDestroy() {
-    // this.timerSub.unsubscribe();
+    console.log('ngondestroy')
+    if (!!this.timerSub){
+      this.timerSub.unsubscribe();
+    }
   }
 }
